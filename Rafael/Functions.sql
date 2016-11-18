4d6b17905eec4fa99f0487ac65f8f8c1fca53bb4
rafarubim <rafael.rubim@bol.com.br>
2016-11-18 20:56:21 -0200

    Me livrei de arquivo desnecessário
    true story.

--- Rafael/Functions.sql
+++ Rafael/Functions.sql
@@ -1,1 +1,24 @@
 -- FUNCTIONS
+
+/***********
+* ObterEtapaSerie:
+*   Recebe uma struct serie e retorna sua data.
+*   (salva em sua prova)
+*********/
+create or replace function ObterEtapaSerie(pSerie in serie%rowtype)
+return date as
+  dataEsperada date;
+begin
+
+  dataEsperada :=
+  (select Data
+    from DataEtapa
+    where pSerie.NumMod = NumMod and
+          pSerie.SexoProva = SexoProva and
+          pSerie.DistProva = DistProva and
+          rownum = pSerie.Etapa
+    order by Data);
+    
+    return dataEsperada;
+end;
+/
